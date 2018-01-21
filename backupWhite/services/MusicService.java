fdc57b92b4fc0f8a32a4405573f8149646a1dfcf
ranjan <ranjansasmal1@github.com>
2018-01-22 00:27:46 +0530

    updated service class on 21.01.2018

--- backupWhite/services/MusicService.java
+++ backupWhite/services/MusicService.java
@@ -1,5 +1,6 @@
 package com.playmusic.services;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,16 +8,21 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.audiofx.PresetReverb;
 import android.media.session.PlaybackState;
 import android.os.Binder;
 import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.telecom.TelecomManager;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import com.playmusic.R;
+import com.playmusic.helpers.Commons;
+import com.playmusic.helpers.Equalizerhelper;
 import com.playmusic.receiver.handsetPlugReceiver;
 
 import java.io.IOException;
@@ -42,7 +48,25 @@ public class MusicService extends Service implements MediaPlayer.OnCompletionLis
     private handsetPlugReceiver mhanHandsetPlugReceiver;
     private final IBinder binder=new LocalBinder();
     private boolean isServiceInitialized=false;
+    private Commons mApp;
+    private Context mContext;
+    private Equalizerhelper mEqualizerHelper;
     private MediaStore.Audio activeAudio;
+    public static final String ACTION_PLAY = "com.playmusic.ACTION_PLAY";
+    public static final String ACTION_PAUSE = "com.playmusic.ACTION_PAUSE";
+    public static final String ACTION_PREVIOUS = "com.playmusic.ACTION_PREVIOUS";
+    public static final String ACTION_NEXT = "com.playmusic.ACTION_NEXT";
+    public static final String ACTION_STOP = "com.playmusic.ACTION_STOP";
+    public static final String LAUNCH_NOW_PLAYING_ACTION = "com.playmusic.services.LAUNCH_NOW_PLAYING_ACTION";
+    private int fiftyHertzLevel = 16;
+    private int oneThirtyHertzLevel = 16;
+    private int threeTwentyHertzLevel = 16;
+    private int eightHundredHertzLevel = 16;
+    private int twoKilohertzLevel = 16;
+    private int fiveKilohertzLevel = 16;
+    private int twelvePointFiveKilohertzLevel = 16;
+    //notification build object.
+    private NotificationCompat.Builder mNotificationBuilder;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -54,13 +78,14 @@ public class MusicService extends Service implements MediaPlayer.OnCompletionLis
         //get media file path as well as its instances.
         try{
             mediaPath=intent.getExtras().getString("mediaFile");
-
+            mContext=getApplicationContext();
             if (requestAudioFocus()==false)
                 stopSelf();
 
             if (mediaPath!=null && mediaPath!="")
                 loadMediaPlayer();
-
+            mApp=(Commons)getApplicationContext();
+            mApp.setService((MusicService) this);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -288,6 +313,120 @@ public class MusicService extends Service implements MediaPlayer.OnCompletionLis
         telephonyManager.listen(phoneStateListener,
                 PhoneStateListener.LISTEN_CALL_STATE);
     }
+    /**
+     * Initializes the equalizer and audio effects for this service session.
+     */
+    public void initAudioFX() {
+
+        try {
+            //Instatiate the equalizer helper object.
+            mEqualizerHelper = new Equalizerhelper(mContext, myMusicPlayer.getAudioSessionId(),
+                    mApp.isEqualizerEnabled());
+
+        } catch (UnsupportedOperationException e) {
+            e.printStackTrace();
+            mEqualizerHelper.setIsEqualizerSupported(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            mEqualizerHelper.setIsEqualizerSupported(false);
+        }
+    }
+    private void applyMediaPlayerEQ(String songId) {
+        if (mEqualizerHelper==null)
+            return;
+        short fiftyHertzBand = mEqualizerHelper.getEqualizer().getBand(50000);
+        short oneThirtyHertzBand = mEqualizerHelper.getEqualizer().getBand(130000);
+        short threeTwentyHertzBand = mEqualizerHelper.getEqualizer().getBand(320000);
+        short eightHundredHertzBand = mEqualizerHelper.getEqualizer().getBand(800000);
+        short twoKilohertzBand = mEqualizerHelper.getEqualizer().getBand(2000000);
+        short fiveKilohertzBand = mEqualizerHelper.getEqualizer().getBand(5000000);
+        short twelvePointFiveKilohertzBand = mEqualizerHelper.getEqualizer().getBand(9000000);
+
+        //int[] eqValues = mApp.getDBAccessHelper().getSongEQValues(songId);
+        //50Hz Band.
+        if (fiftyHertzLevel==16) {
+            mEqualizerHelper.getEqualizer().setBandLevel(fiftyHertzBand, (short) 0);
+        }
+//        else if (fiftyHertzLevel < 16) {
+//
+//            if (eqValues[0]==0) {
+//                mEqualizerHelper.getEqualizer().setBandLevel(fiftyHertzBand, (short) -1500);
+//            } else {
+//                mEqualizerHelper.getEqualizer().setBandLevel(fiftyHertzBand, (short) (-(16-eqValues[0])*100));
+//            }
+//
+//        } else if (eqValues[0] > 16) {
+//            mEqualizerHelper.getEqualizer().setBandLevel(fiftyHertzBand, (short) ((eqValues[0]-16)*100));
+//        }
+        //130Hz Band.
+        if (oneThirtyHertzBand==16) {
+            mEqualizerHelper.getEqualizer().setBandLevel(oneThirtyHertzBand, (short) 0);
+        }
+//        else if (eqValues[1] < 16) {
+//
+//            if (eqValues[1]==0) {
+//                mEqualizerHelper.getEqualizer().setBandLevel(oneThirtyHertzBand, (short) -1500);
+//            } else {
+//                mEqualizerHelper.getEqualizer().setBandLevel(oneThirtyHertzBand, (short) (-(16-eqValues[1])*100));
+//            }
+//
+//        } else if (eqValues[1] > 16) {
+//            mEqualizerHelper.getEqualizer().setBandLevel(oneThirtyHertzBand, (short) ((eqValues[1]-16)*100));
+//        }
+        //320Hz Band.
+        if (threeTwentyHertzLevel==16) {
+            mEqualizerHelper.getEqualizer().setBandLevel(threeTwentyHertzBand, (short) 0);
+        }
+//        else if (eqValues[2] < 16) {
+//
+//            if (eqValues[2]==0) {
+//                mEqualizerHelper.getEqualizer().setBandLevel(threeTwentyHertzBand, (short) -1500);
+//            } else {
+//                mEqualizerHelper.getEqualizer().setBandLevel(threeTwentyHertzBand, (short) (-(16-eqValues[2])*100));
+//            }
+//
+//        } else if (eqValues[2] > 16) {
+//            mEqualizerHelper.getEqualizer().setBandLevel(threeTwentyHertzBand, (short) ((eqValues[2]-16)*100));
+//        }
+        mEqualizerHelper.getVirtualizer().setStrength((short) 2);
+        mEqualizerHelper.getBassBoost().setStrength((short) 3);
+//        if (eqValues[9]==0) {
+//            mEqualizerHelper.getReverb().setPreset(PresetReverb.PRESET_NONE);
+//        }else if (eqValues[9]==1) {
+//            mEqualizerHelper.getReverb().setPreset(PresetReverb.PRESET_LARGEHALL);
+//        } else if (eqValues[9]==2) {
+//            mEqualizerHelper.getReverb().setPreset(PresetReverb.PRESET_LARGEROOM);
+//        } else if (eqValues[9]==3) {
+//            mEqualizerHelper.getReverb().setPreset(PresetReverb.PRESET_MEDIUMHALL);
+//        } else if (eqValues[9]==4) {
+//            mEqualizerHelper.getReverb().setPreset(PresetReverb.PRESET_MEDIUMROOM);
+//        } else if (eqValues[9]==5) {
+//            mEqualizerHelper.getReverb().setPreset(PresetReverb.PRESET_SMALLROOM);
+//        } else if (eqValues[9]==6) {
+//            mEqualizerHelper.getReverb().setPreset(PresetReverb.PRESET_PLATE);
+//        }
+
+    }
+
+    public NotificationCompat BuildNotification(){
+        mNotificationBuilder = new NotificationCompat.Builder(mContext);
+        mNotificationBuilder.setOngoing(true);
+        mNotificationBuilder.setAutoCancel(false);
+       // mNotificationBuilder.setSmallIcon(R.drawable.notif_icon);
+        //Open up the player screen when the user taps on the notification.
+        Intent launchNowPlayingIntent = new Intent();
+        launchNowPlayingIntent.setAction(MusicService.LAUNCH_NOW_PLAYING_ACTION);
+        PendingIntent launchNowPlayingPendingIntent = PendingIntent.getBroadcast(mContext.getApplicationContext(), 0, launchNowPlayingIntent, 0);
+        mNotificationBuilder.setContentIntent(launchNowPlayingPendingIntent);
+
+        return null;
+    }
+    public Equalizerhelper getEqualizerHelper() {
+        return mEqualizerHelper;
+    }
+    public MediaPlayer getMediaPlayer() {
+        return myMusicPlayer;
+    }
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -299,6 +438,7 @@ public class MusicService extends Service implements MediaPlayer.OnCompletionLis
         if (phoneStateListener != null) {
             telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
         }
+
         unregisterReceiver(playNewAudio);
         unregisterReceiver(mhanHandsetPlugReceiver);
         removeAudioFocus();
